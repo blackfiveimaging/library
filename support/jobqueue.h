3e6d7c6e027712c14204a6c8d7a48ab36ac05851
Alastair Robinson <amr@amr-desktop.(none)>
2009-09-27 01:25:06 +0100

    Dispatcher and worker threads added to jobqueue

--- support/jobqueue.h
+++ support/jobqueue.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <queue>
+#include <list>
 
 #include "support/thread.h"
 #include "support/ptmutex.h"
@@ -58,7 +59,7 @@ class JobQueue : public ThreadCondition
 		if(j)
 		{
 			j->Run();
-			delete j;
+//			delete j;		// Safer to let the job handle its own demise...
 			return(true);
 		}
 		return(false);
@@ -85,6 +86,111 @@ class JobQueue : public ThreadCondition
 	protected:
 	std::queue<Job *> myqueue;
 };
+
+
+class Worker : public ThreadFunction, public PTMutex
+{
+	public:
+	Worker(JobQueue &queue)
+		: ThreadFunction(), PTMutex(), queue(queue), thread(this), cancelled(false)
+	{
+//		std::cerr << "Starting worker thread..." << std::endl;
+		thread.Start();
+	}
+	virtual ~Worker()
+	{
+//		std::cerr << "Destructing worker thread..." << std::endl;
+		cancelled=true;
+		while(!thread.TestFinished())
+		{
+			// We obtain this mutex to avoid a busy wait - the subthread holds it while processing a job.
+			ObtainMutex();
+			queue.ObtainMutex();
+			queue.Broadcast();
+			queue.ReleaseMutex();
+			ReleaseMutex();
+		}
+	}
+	virtual int Entry(Thread &t)
+	{
+//		std::cerr << "Worker thread running..." << std::endl;
+		do
+		{
+//			std::cerr << "Obtaining mutex" << std::endl;
+			queue.ObtainMutex();
+			while(queue.IsEmpty())
+			{
+//				std::cerr << "Waiting for a job" << std::endl;
+				queue.WaitCondition();
+//				std::cerr << "Signal received" << std::endl;
+				if(cancelled)
+				{
+//					std::cerr << "Received cancellation signal" << std::endl;
+					queue.ReleaseMutex();
+					return(0);
+				}
+			}
+//			std::cerr << "Releasing mutex" << std::endl;
+			queue.ReleaseMutex();
+//			std::cerr << "Running job" << std::endl;
+
+			// Obtain a per-thread mutex while running the job, so the destructor can avoid a busy-wait.
+			ObtainMutex();
+			queue.Dispatch();
+			ReleaseMutex();
+
+			// Send a pulse to say the thread's adopting a new job
+			queue.ObtainMutex();
+			queue.Broadcast();
+			queue.ReleaseMutex();
+		} while(!cancelled);
+//		std::cerr << "Cancelled" << std::endl;
+		return(0);
+	}
+	protected:
+	JobQueue &queue;
+	Thread thread;
+	bool cancelled;
+};
+
+
+class JobDispatcher : public JobQueue
+{
+	public:
+	JobDispatcher(int threads) : JobQueue()
+	{
+		for(int i=0;i<threads;++i)
+			threadlist.push_back(new Worker(*this));
+	}
+	virtual ~JobDispatcher()
+	{
+		while(!threadlist.empty())
+		{
+			Worker *thread=threadlist.front();
+			delete thread;
+			threadlist.pop_front();
+		}
+	}
+	virtual void WaitCompletion()
+	{
+		ObtainMutex();
+		std::list<Worker *>::iterator it=threadlist.begin();
+		while(it!=threadlist.end())
+		{
+			while(JobCount())
+			{
+//				std::cerr << "(" << JobCount() << " jobs remaining...)" << std::endl;
+				WaitCondition();
+			}
+			++it;
+		}
+		ReleaseMutex();
+	}
+	protected:
+	std::list<Worker *> threadlist;
+	friend class Worker;
+};
+
 
 #endif
 
